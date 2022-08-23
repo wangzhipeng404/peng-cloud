74da0aeb686360da16ad3cd0bfa4b9d087b28260
wangzhipeng404 <wangzhipeng404@gmail.com>
2022-08-23 23:47:58 +0800

    分开入口打包，减小打包体积

--- src/mobile.js
+++ src/mobile.js
@@ -1,25 +1,17 @@
-import('@ant-design-vue/pro-layout/dist/style.less')
-import('vant/lib/index.less')
-
-async function initWeb () {
+import('vant/lib/index.css')
+async function initMobile () {
   const { initImportMap } = await import('./utils/importmap')
   await initImportMap()
   const { createApp, defineAsyncComponent } = await import('vue')
-  const { ConfigProvider } = await import('ant-design-vue');
   const { default: vant } = await import('vant')
-  const {default: ProLayout } = await import('@ant-design-vue/pro-layout')
-  const { PageContainer } = await import('@ant-design-vue/pro-layout')
-  const {default: router} = await import('./router/index')
-  const {default: App} = await import('./App.vue')
+  const {default: router } = await import('./router/mobile')
   const { findComponents } = await import('./service/compoment')
   const { createComponent }  = await import('./utils/parser')
+  const {default: App} = await import('./AppMobile.vue')
 
   const app = createApp(App)
   app.use(router)
-  app.use(ConfigProvider)
   app.use(vant)
-  app.use(ProLayout)
-  app.use(PageContainer)
   const res = await findComponents()
   res.map(item => {
     app.component(item.key, defineAsyncComponent(() => createComponent(item.script, item.key)))
@@ -27,6 +19,4 @@ async function initWeb () {
   app.mount('#app');
 }
 
-initWeb()
-
-
+initMobile()
