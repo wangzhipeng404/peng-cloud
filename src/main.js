6818873f2e8ae1e8ef623f0793dd0c042eaa6d13
wangzhipeng404 <wangzhipeng404@gmail.com>
2022-08-30 00:02:45 +0800

    添加tmagic相关依赖

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import('@ant-design-vue/pro-layout/dist/style.less')
 import('vant/lib/index.less')
+import "element-plus/dist/index.css";
+import "@tmagic/editor/dist/style.css";
+import "@tmagic/form/dist/style.css";
 import { initGlobal } from '@/utils/initGlobal'
 import { importJS } from '@/utils/importJS'
 async function initWeb () {
@@ -8,6 +11,10 @@ async function initWeb () {
   const { initImportMap } = await import('./utils/importmap')
   await initImportMap()
   const { createApp, defineAsyncComponent } = await import('vue')
+  const { default:  ElementPlus } = await import("element-plus")
+  const { default: zhCn } = await import("element-plus/es/locale/lang/zh-cn");
+  const { default: MagicEditor } =  await "@tmagic/editor";
+  const { default: MagicForm } =  await("@tmagic/form");
   const { ConfigProvider } = await import('ant-design-vue');
   const { default: vant } = await import('vant')
   const {default: ProLayout } = await import('@ant-design-vue/pro-layout')
@@ -23,6 +30,9 @@ async function initWeb () {
   app.use(vant)
   app.use(ProLayout)
   app.use(PageContainer)
+  app.use(ElementPlus, { locale: zhCn })
+  app.use(MagicEditor)
+  app.use(MagicForm)
   const res = await findComponents()
   res.map(item => {
     app.component(item.key, defineAsyncComponent(() => createComponent(item.script, item.key)))
---
+++ src/views/page-editor/tmagic.vue
@@ -0,0 +1,64 @@
+<template>
+  <m-editor
+    v-model="data"
+    :menu="menu"
+    :runtime-url="runtimeUrl"
+    :props-configs="propsConfigs"
+    :props-values="propsValues"
+    :component-group-list="componentGroupList"
+  >
+  </m-editor>
+</template>
+
+<script>
+  import { defineComponent, ref } from "vue";
+
+  export default defineComponent({
+    name: "App",
+
+    setup() {
+      return {
+        menu: ref({
+          left: [
+            // 顶部左侧菜单按钮
+          ],
+          center: [
+            // 顶部中间菜单按钮
+          ],
+          right: [
+            // 顶部右侧菜单按钮
+          ],
+        }),
+
+        data: ref({
+          // 初始化页面数据
+        }),
+
+        runtimeUrl: "/runtime/vue3/playground.html",
+        propsConfigs: [
+          // 组件属性列表
+        ],
+        propsValues: [
+          // 组件默认值
+        ],
+
+        componentGroupList: ref([
+          // 组件列表
+        ]),
+      };
+    },
+  });
+</script>
+
+<style lang="scss">
+  #app {
+    width: 100%;
+    height: 100%;
+    display: flex;
+  }
+
+  .m-editor {
+    flex: 1;
+    height: 100%;
+  }
+</style>
